79e27ded863b913580e27bbe70317434a411d467
minel1410 <msalihagic@hotmail.com>
2024-08-25 00:24:20 +0200

    Test

--- api/main.py
+++ api/main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 @app.get("/")
-def read_root():
-    return {"ping": "Pong!"}
-
+async def ping_pong():
+    return "ping pong"
